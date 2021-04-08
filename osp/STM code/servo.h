eba41410252f2ce02fc57271f75fd9190d6278da
TomAndrzejewski <tomasz.andrzejewski4@o2.pl>
2021-04-08 17:43:13 +0200

    Scalono wszystkie funkcjonalnosci

---
+++ osp/STM code/servo.h
@@ -0,0 +1,26 @@
+ /*
+ * servo.h
+ *
+ *  Created on: Mar 18, 2021
+ *      Author: Tomek
+ */
+
+
+
+#ifndef INC_SERVO_H_
+
+#include <stdint.h>
+
+
+
+/*
+struct
+{
+uint16_t MIN;
+uint16_t MAX;
+}PWMdata;
+
+*/
+void set_ang(uint8_t mode);
+
+#endif /* INC_SERVO_H_ */
